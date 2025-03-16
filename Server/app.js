34d77dd4e0d6aa0e7baa74d83b2b057cb5b988fe
Anish_Saini <anishsaini9098@gmail.com>
2025-03-17 02:39:14 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,7 @@ import os from "os";
 
 import DBConnection from "./Database/database.js";
 import Router from "./Routes/Router.js";
-import ConsultRouter from "./Routes/ConsultRoutes.js";
+import ConsultRouter from "./Routes/COnsultRoutes.js";
 import StudentRouter from "./Routes/StudentRouter.js";
 
 dotenv.config();
